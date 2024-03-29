3e8b2a7ad44b59309c86c5d1b95f71b8cdcf61be
casamassa <marcelocasamassa@gmail.com>
2024-03-28 22:27:53 -0300

    Change chunk size to fix a bug in HuggingFace token size. Add a pdf file example to tests.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_pdf_text(pdf_docs):
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
+        chunk_size=800,
         chunk_overlap=200,
         length_function=len
     )
